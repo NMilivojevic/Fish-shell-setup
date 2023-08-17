df3b472125d642c65afa0b29129133222e88b1b7
NMilivojevic <nevmilivojevic@gmail.com>
2023-08-17 12:13:30 +0200

    test for linux and mac

--- fish_install.sh
+++ fish_install.sh
@@ -1,12 +1,18 @@
-echo "installing fish..."
-sudo apt install fish
-echo "done."
-echo "setting up fish as default shell..."
-chsh -s /usr/bin/fish
-echo "done."
-echo "creating config directory..."
-mkdir -p ~/.config/fish
-echo "done."
-echo "setting up your config fish file. You can modify this file to your liking."
-echo "set -g -x fish_greeting ''" > ~/.config/fish/config.fish
-echo "All set! Make sure to reboot in order for changes to take effect."
+# echo "installing fish..."
+# sudo apt install fish
+# echo "done."
+# echo "setting up fish as default shell..."
+# chsh -s /usr/bin/fish
+# echo "done."
+# echo "creating config directory..."
+# mkdir -p ~/.config/fish
+# echo "done."
+# echo "setting up your config fish file. You can modify this file to your liking."
+# echo "set -g -x fish_greeting ''" > ~/.config/fish/config.fish
+# echo "All set! Make sure to reboot in order for changes to take effect."
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+     echo "linux system"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+     echo "mac os system"
+fi
